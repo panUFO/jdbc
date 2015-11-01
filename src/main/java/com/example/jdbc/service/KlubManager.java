8184afa350d615d1fda2d84dc9f98a7885d9aa64
Kamil Liwinski <kliwinski93@gmail.com>
2015-11-01 21:32:54 +0100

    dodanie klasy zespol_has_koncert cd klasy KlubManager

--- src/main/java/com/example/jdbc/service/KlubManager.java
+++ src/main/java/com/example/jdbc/service/KlubManager.java
@@ -28,8 +28,15 @@ public class KlubManager implements KlubDAO {
             ")";
 
     private PreparedStatement addKlubStmt;
-    private PreparedStatement deleteAllKlubsStmt;
     private PreparedStatement getAllKlubsStmt;
+    private PreparedStatement getKlubByIdStmt;
+    private PreparedStatement getKlubByMiastoStmt;
+    private PreparedStatement getKlubByNazwaStmt;
+    private PreparedStatement getKlubByIMiejscStmt;
+    private PreparedStatement deleteKlubStmt;
+    private PreparedStatement deleteAllKlubsStmt;
+    private PreparedStatement updateKlubStmt;
+
 
     private Statement statement;
 
@@ -53,9 +60,14 @@ public class KlubManager implements KlubDAO {
                 statement.executeUpdate(createTableKlub);
 
             addKlubStmt = connection.prepareStatement("INSERT INTO klub (klub_miasto, klub_nazwa_klubu, klub_ilosc_miejsc) VALUES (?, ?, ?)");
-            deleteAllKlubsStmt = connection.prepareStatement("DELETE FROM klub");
             getAllKlubsStmt = connection.prepareStatement("SELECT klub_id, klub_miasto, klub_nazwa_klubu, klub_ilosc_miejsc FROM klub");
-
+            getKlubByIdStmt = connection.prepareStatement("SELECT * FROM klub WHERE klub_id = ?");
+            getKlubByMiastoStmt = connection.prepareStatement("SELECT * FROM klub WHERE klub_miasto = ?");
+            getKlubByNazwaStmt = connection.prepareStatement("SELECT * FROM klub WHERE klub_nazwa_klubu = ?");
+            getKlubByIMiejscStmt = connection.prepareStatement("SELECT * FROM klub WHERE klub_ilosc_miejsc = ?");
+            deleteKlubStmt = connection.prepareStatement("DELETE FROM klub WHERE klub_id = ?");
+            deleteAllKlubsStmt = connection.prepareStatement("DELETE FROM klub");
+            updateKlubStmt = connection.prepareStatement("UPDATE klub SET klub_miasto = ?, klub_nazwa_klubu = ?, klub_ilosc_miejsc = ? WHERE klub_id = ?");
 
         }
 
@@ -67,53 +79,163 @@ public class KlubManager implements KlubDAO {
             return connection;
        }
 
-        void clearKlubs() {
-            try {
-                deleteAllKlubsStmt.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+
+    public int addKlub (Klub klub) {
+        int count = 0;
+        try {
+            addKlubStmt.setString(1, klub.getMiasto());
+            addKlubStmt.setString(2, klub.getNazwa());
+            addKlubStmt.setInt(3, klub.getIlosc_miejsc());
+
+            count = addKlubStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return count;
+    }
 
-        public int addKlub (Klub klub) {
-            int count = 0;
-            try {
-                addKlubStmt.setString(1, klub.getMiasto());
-                addKlubStmt.setString(2, klub.getNazwa());
-                addKlubStmt.setInt(3, klub.getIlosc_miejsc());
+    public List<Klub> getAllKlubs(){
+        List<Klub> klub = new ArrayList<Klub>();
 
-                count = addKlubStmt.executeUpdate();
+        ResultSet rs = null;
+        try {
+            rs = getAllKlubsStmt.executeQuery();
 
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            return count;
-        }
+            while (rs.next()) {
+                Klub k = new Klub();
+                k.setID(rs.getInt("klub_id"));
+                k.setMiasto(rs.getString("klub_miasto"));
+                k.setNazwa(rs.getString("klub_nazwa_klubu"));
+                k.setIlosc_miejsc(rs.getInt("klub_ilosc_miejsc"));
+                klub.add(k);
 
-        public List<Klub> getAllKlubs(){
-            List<Klub> klub = new ArrayList<Klub>();
-
-            ResultSet rs = null;
-            try {
-                rs = getAllKlubsStmt.executeQuery();
-
-                while (rs.next()) {
-                    Klub k = new Klub();
-                    k.setID(rs.getInt("klub_id"));
-                    k.setMiasto(rs.getString("klub_miasto"));
-                    k.setNazwa(rs.getString("klub_nazwa_klubu"));
-                    k.setIlosc_miejsc(rs.getInt("klub_ilosc_miejsc"));
-                    klub.add(k);
-
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
 
-            return klub;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
+        return klub;
+    }
+
+    public Klub getKlubById (Klub klub) {
+
+        try {
+            getKlubByIdStmt.setInt(1, klub.getID());
+            ResultSet rs = getKlubByIdStmt.executeQuery();
+
+            while (rs.next()){
+                klub = new Klub(rs.getString("klub_miasto"), rs.getString("klub_nazwa_klubu"), rs.getInt("klub_ilosc_miejsc"));
+                klub.setID(rs.getInt("klub_id"));
+                return klub;
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Klub> getKlubByMiasto (Klub klub) {
+        List<Klub> kluby = new ArrayList<Klub>();
+
+        try {
+            ResultSet rs = getKlubByMiastoStmt.executeQuery();
+            while (rs.next()) {
+                Klub k = new Klub();
+                k.setID(rs.getInt("klub_id"));
+                k.setMiasto(rs.getString("klub_miasto"));
+                k.setNazwa(rs.getString("klub_nazwa_klubu"));
+                k.setIlosc_miejsc(rs.getInt("klub_ilosc_miejsc"));
+                kluby.add(k);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return kluby;
+    }
+
+    public List<Klub> getKlubByNazwa (Klub klub) {
+        List<Klub> kluby = new ArrayList<Klub>();
+
+        try {
+            ResultSet rs = getKlubByNazwaStmt.executeQuery();
+            while (rs.next()) {
+                Klub k = new Klub();
+                k.setID(rs.getInt("klub_id"));
+                k.setMiasto(rs.getString("klub_miasto"));
+                k.setNazwa(rs.getString("klub_nazwa_klubu"));
+                k.setIlosc_miejsc(rs.getInt("klub_ilosc_miejsc"));
+                kluby.add(k);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return kluby;
+    }
+
+    public List<Klub> getKlubByIMiejsc (Klub klub) {
+        List<Klub> kluby = new ArrayList<Klub>();
+
+        try {
+            ResultSet rs = getKlubByIMiejscStmt.executeQuery();
+            while (rs.next()) {
+                Klub k = new Klub();
+                k.setID(rs.getInt("klub_id"));
+                k.setMiasto(rs.getString("klub_miasto"));
+                k.setNazwa(rs.getString("klub_nazwa_klubu"));
+                k.setIlosc_miejsc(rs.getInt("klub_ilosc_miejsc"));
+                kluby.add(k);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return kluby;
+    }
+
+    public int deleteKlub (Klub klub) {
+        int count = 0;
+        try {
+
+            deleteKlubStmt.setInt(1, klub.getID());
+            count = addKlubStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    void clearKlubs() {
+        try {
+            deleteAllKlubsStmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int updateKlub (Klub klub) {
+        int count = 0;
+        try {
+            updateKlubStmt.setString(1, klub.getMiasto());
+            updateKlubStmt.setString(2, klub.getNazwa());
+            updateKlubStmt.setInt(3, klub.getIlosc_miejsc());
+
+            count = updateKlubStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
 
 }
 
