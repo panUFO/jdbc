d7e3e1d836e3d5cefe13dcd15d8c118e2fb05d6d
Kamil Liwinski <kliwinski93@gmail.com>
2015-11-01 15:57:55 +0100

    Utworzenie repo, dodanie pierwszych klas

---
+++ src/main/java/com/example/jdbc/domain/KlubDAO.java
@@ -0,0 +1,8 @@
+package main.java.com.example.jdbc.domain;
+
+/**
+ * Created by Ufo on 2015-11-01.
+ */
+public interface KlubDAO {
+
+}
---
+++ src/main/java/com/example/jdbc/domain/KoncertDAO.java
@@ -0,0 +1,7 @@
+package main.java.com.example.jdbc.domain;
+
+/**
+ * Created by Ufo on 2015-11-01.
+ */
+public interface KoncertDAO {
+}
